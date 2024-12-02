f41cb6916916b341e31351ad170716e00ea78fde
Wolves <cstingl@protonmail.com>
2024-12-02 10:09:36 -0500

    day01: Part one initial basic solutuion

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,11 +1,27 @@
 fn main() {
-    let input = include_str!("./input.txt");
+    let input = include_str!("./input1.txt");
     let output = process(input);
     dbg!(output);
 }
 
 fn process(input: &str) -> String {
-    "todo!()".to_string()
+    let mut left: Vec<i32> = vec![];
+    let mut right: Vec<i32> = vec![];
+
+    for line in input.lines() {
+        let mut items = line.split_whitespace();
+        left.push(items.next().unwrap().parse::<i32>().unwrap());
+        right.push(items.next().unwrap().parse::<i32>().unwrap());
+    }
+
+    left.sort();
+    right.sort();
+
+    let result: i32 = std::iter::zip(left, right)
+        .map(|(l, r)| (l - r).abs())
+        .sum();
+
+    result.to_string()
 }
 
 #[cfg(test)]
@@ -13,8 +29,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = process("");
-        assert_eq!(result, "todo!()".to_string());
+    fn test_process() {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("11", process(input));
     }
 }
