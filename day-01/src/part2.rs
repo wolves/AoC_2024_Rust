fcbdfa92c035def13e716a6295719656c091a089
Wolves <cstingl@protonmail.com>
2024-12-02 14:39:10 -0500

    day01: Solution for part2

--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,5 +1,19 @@
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 00 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut left = vec![];
+    let mut right = vec![];
+
+    for line in input.lines() {
+        let mut items = line.split_whitespace();
+        left.push(items.next().unwrap().parse::<usize>().unwrap());
+        right.push(items.next().unwrap().parse::<usize>().unwrap());
+    }
+
+    let result: usize = left
+        .iter()
+        .map(|number| number * right.iter().filter(|r| &number == r).count())
+        .sum();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -8,9 +22,13 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("TODO: Build Test");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("31", process(input)?);
         Ok(())
     }
 }
