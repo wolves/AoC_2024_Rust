7b232a0a004fa8f1d16b307cb41d6f13f709b4c2
Wolves <cstingl@protonmail.com>
2024-12-02 14:18:45 -0500

    Shift Day01 Part1 over to new project structure

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,41 +1,9 @@
-fn main() {
-    let input = include_str!("./input1.txt");
-    let output = process(input);
-    dbg!(output);
-}
+ use day_01::part1::process;
+ use miette::Context;
 
-fn process(input: &str) -> String {
-    let mut left: Vec<i32> = vec![];
-    let mut right: Vec<i32> = vec![];
-
-    for line in input.lines() {
-        let mut items = line.split_whitespace();
-        left.push(items.next().unwrap().parse::<i32>().unwrap());
-        right.push(items.next().unwrap().parse::<i32>().unwrap());
-    }
-
-    left.sort();
-    right.sort();
-
-    let result: i32 = std::iter::zip(left, right)
-        .map(|(l, r)| (l - r).abs())
-        .sum();
-
-    result.to_string()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_process() {
-        let input = "3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
-        assert_eq!("11", process(input));
-    }
-}
+ fn main() -> miette::Result<()> {
+     let file = include_str!("../../input1.txt");
+     let result = process(file).context("process part 1")?;
+     println!("{}", result);
+     Ok(())
+ }
--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,3 +1,10 @@
-fn main() {
-    print!("todo")
-}
+ use day_01::part2::process;
+ use miette::Context;
+
+ fn main() -> miette::Result<()> {
+     let file = include_str!("../../input2.txt");
+     let result = process(file).context("process part 2")?;
+     println!("{}", result);
+     Ok(())
+ }
+
