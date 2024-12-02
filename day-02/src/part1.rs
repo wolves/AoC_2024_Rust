2a4a85514d632da7c0ce00387082e8a93305f392
Wolves <cstingl@protonmail.com>
2024-12-02 17:40:31 -0500

    day2: Part 1 in progress

--- day-02/src/part1.rs
+++ day-02/src/part1.rs
@@ -1,5 +1,31 @@
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 00 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let reports = parse_reports(input);
+
+    let result = reports.iter().filter(|x| is_safe_report(x)).count();
+
+    Ok(result.to_string())
+}
+
+fn parse_reports(input: &str) -> Vec<Vec<i32>> {
+    input
+        .lines()
+        .map(|x| {
+            x.split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
+}
+
+fn is_safe_report(input: &[i32]) -> bool {
+    let sig = (input[0] - input[1]).signum();
+
+    // input
+    //     .windows(2)
+    //     .map()
+    dbg!(sig);
+
+    false
 }
 
 #[cfg(test)]
@@ -8,9 +34,13 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("TODO: Build Test");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!("2", process(input)?);
         Ok(())
     }
 }
