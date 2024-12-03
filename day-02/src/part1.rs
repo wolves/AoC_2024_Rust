72a53b03138d045334460a38406591531df7b90b
wolves <cstingl@protonmail.com>
2024-12-03 06:23:25 -0500

    day2(refactor): Add Itertools and refactor day2 to use direction enum with match

--- day-02/src/part1.rs
+++ day-02/src/part1.rs
@@ -1,12 +1,24 @@
+use itertools::Itertools;
+
+type Report = Vec<i32>;
+
+enum Direction {
+    Increasing,
+    Decreasing,
+}
+
 pub fn process(input: &str) -> miette::Result<String> {
     let reports = parse_reports(input);
 
-    let result = reports.iter().filter(|x| is_safe_report(x)).count();
+    let result = reports
+        .iter()
+        .filter(|report| is_safe_report(report).is_ok())
+        .count();
 
     Ok(result.to_string())
 }
 
-fn parse_reports(input: &str) -> Vec<Vec<i32>> {
+fn parse_reports(input: &str) -> Vec<Report> {
     input
         .lines()
         .map(|x| {
@@ -17,13 +29,62 @@ fn parse_reports(input: &str) -> Vec<Vec<i32>> {
         .collect::<Vec<_>>()
 }
 
-fn is_safe_report(input: &[i32]) -> bool {
-    let sig = (input[0] - input[1]).signum();
+fn is_safe_report(input: &Report) -> Result<(), String> {
+    let mut direction: Option<Direction> = None;
 
-    input
-        .windows(2)
-        .map(|w| w[0] - w[1])
-        .all(|x| (1..=3).contains(&x.abs()) && x.signum() == sig)
+    for (a, b) in input.iter().tuple_windows() {
+        let diff = a - b;
+        match diff.signum() {
+            -1 => match direction {
+                Some(Direction::Increasing) => {
+                    return Err(format!("{}, {} switched to increasing", a, b));
+                }
+                Some(Direction::Decreasing) => {
+                    if !(1..=3).contains(&diff.abs()) {
+                        return Err(format!("{}, {} diff value is {}", a, b, diff.abs()));
+                    } else {
+                        continue;
+                    }
+                }
+                None => {
+                    if !(1..=3).contains(&diff.abs()) {
+                        return Err(format!("{}, {} diff value is {}", a, b, diff.abs()));
+                    } else {
+                        direction = Some(Direction::Decreasing);
+                        continue;
+                    }
+                }
+            },
+            1 => match direction {
+                Some(Direction::Increasing) => {
+                    if !(1..=3).contains(&diff.abs()) {
+                        return Err(format!("{}, {} diff value is {}", a, b, diff.abs()));
+                    } else {
+                        continue;
+                    }
+                }
+                Some(Direction::Decreasing) => {
+                    return Err(format!("{}, {} switched to decreasing", a, b));
+                }
+                None => {
+                    if !(1..=3).contains(&diff.abs()) {
+                        return Err(format!("{}, {} diff value is {}", a, b, diff.abs()));
+                    } else {
+                        direction = Some(Direction::Increasing);
+                        continue;
+                    }
+                }
+            },
+            0 => {
+                return Err(format!("{}, {} diff was 0", a, b));
+            }
+            _ => {
+                panic!("Should never have a non -1, 1, 0 number");
+            }
+        }
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
