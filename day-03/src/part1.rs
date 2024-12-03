fc7c49da25e94c38dfa1919e302721780fa3aa43
Wolves <cstingl@protonmail.com>
2024-12-03 13:24:33 -0500

    day3: Init Commit - Beginning work

---
+++ day-03/src/part1.rs
@@ -0,0 +1,16 @@
+pub fn process(input: &str) -> miette::Result<String> {
+    let result = "";
+    return Ok(result.to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!("161", process(input)?);
+        Ok(())
+    }
+}
