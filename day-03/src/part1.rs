1bff71e6aab6f597ff7961eb8f3758b8480302fc
Wolves <cstingl@protonmail.com>
2024-12-03 14:51:07 -0500

    day3(part1): Solution

--- day-03/src/part1.rs
+++ day-03/src/part1.rs
@@ -1,6 +1,40 @@
+use miette::miette;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, anychar},
+    multi::{many1, many_till},
+    sequence::{delimited, separated_pair},
+    IResult, Parser,
+};
+
 pub fn process(input: &str) -> miette::Result<String> {
-    let result = "";
-    return Ok(result.to_string());
+    let (_input, instructions) = parse(input).map_err(|e| miette!("parse failed {}", e))?;
+    let result: u32 = instructions
+        .iter()
+        .map(|ins| match ins {
+            Instruction::Mul(a, b) => a * b,
+        })
+        .sum();
+    Ok(result.to_string())
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Mul(u32, u32),
+}
+
+fn instruction(input: &str) -> IResult<&str, Instruction> {
+    let (input, _) = tag("mul")(input)?;
+    let (input, pair) = delimited(
+        tag("("),
+        separated_pair(complete::u32, tag(","), complete::u32),
+        tag(")"),
+    )(input)?;
+    Ok((input, Instruction::Mul(pair.0, pair.1)))
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<Instruction>> {
+    many1(many_till(anychar, instruction).map(|(_discard, ins)| ins))(input)
 }
 
 #[cfg(test)]
