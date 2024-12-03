c3f0e32b1c91f03067ac7dccd73b6346580513b4
Wolves <cstingl@protonmail.com>
2024-12-03 14:51:29 -0500

    day3(part2): Copy over from part1 and begin working

--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -1,5 +1,49 @@
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 00 - part 2");
+use miette::miette;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, anychar},
+    multi::{many1, many_till},
+    sequence::{delimited, separated_pair},
+    IResult, Parser,
+};
+
+pub fn process(input: &str) -> miette::Result<String> {
+    let (_input, instructions) = parse(input).map_err(|e| miette!("parse failed {}", e))?;
+    let result: u32 = instructions
+        .iter()
+        .map(|ins| match ins {
+            Instruction::Mul(a, b) => a * b,
+            Instruction::Do => todo!(),
+            Instruction::Dont => todo!(),
+        })
+        .sum();
+    Ok(result.to_string())
+}
+
+enum ShoudProcess {
+    Do,
+    Dont,
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Mul(u32, u32),
+    Do,
+    Dont,
+}
+
+fn mul(input: &str) -> IResult<&str, Instruction> {
+    let (input, _) = tag("mul")(input)?;
+    let (input, pair) = delimited(
+        tag("("),
+        separated_pair(complete::u32, tag(","), complete::u32),
+        tag(")"),
+    )(input)?;
+    Ok((input, Instruction::Mul(pair.0, pair.1)))
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<Instruction>> {
+    many1(many_till(anychar, mul).map(|(_discard, ins)| ins))(input)
 }
 
 #[cfg(test)]
@@ -8,9 +52,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("TODO: Build Test");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!("48", process(input)?);
         Ok(())
     }
 }
