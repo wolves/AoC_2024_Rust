62c81aec5fb249f42f0b7fc4bb1304c489bba64f
wolves <cstingl@protonmail.com>
2024-12-04 21:16:41 -0500

    day04(part1): Solution

--- day-04/src/part1.rs
+++ day-04/src/part1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use glam::IVec2;
 
 const DIRECTIONS: [[IVec2; 3]; 8] = [
@@ -11,8 +13,39 @@ const DIRECTIONS: [[IVec2; 3]; 8] = [
     [IVec2::new(-1, 1), IVec2::new(-2, 2), IVec2::new(-3, 3)], // SouthWest
 ];
 
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 00 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let positions = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, value)| (IVec2::new(x as i32, y as i32), value))
+        })
+        .collect::<HashMap<IVec2, char>>();
+
+    let mas = ['M', 'A', 'S'];
+    let result: usize = positions
+        .iter()
+        .filter(|(_position, value)| **value == 'X')
+        .map(|(position, value)| {
+            let count = DIRECTIONS
+                .iter()
+                .map(|potential_mas_pos| {
+                    potential_mas_pos
+                        .iter()
+                        .map(|offset| positions.get(&(position + offset)))
+                        .enumerate()
+                        .all(|(index, value)| mas.get(index) == value)
+                })
+                .filter(|b| *b)
+                .count();
+
+            count
+        })
+        .sum();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
