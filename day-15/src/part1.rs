e93f7004419b44aac6827d54201a981ee7188600
Wolves <cstingl@protonmail.com>
2024-12-16 13:19:51 -0500

    day-15(part1): Create day begin setting up parsing

---
+++ day-15/src/part1.rs
@@ -0,0 +1,78 @@
+use std::collections::HashMap;
+
+use glam::IVec2;
+use nom::IResult;
+use nom_locate::LocatedSpan;
+
+pub fn process(input: &str) -> miette::Result<String> {
+    let input = input.trim();
+    todo!("day 00 - part 1");
+}
+
+pub type Span<'a> = LocatedSpan<&'a str>;
+
+#[derive(Debug, Clone)]
+enum Object {
+    Wall,
+    Box,
+    Robot,
+}
+
+fn object_pos(input: Span) -> IResult<Span, (IVec2, Object)> {
+    todo!()
+}
+
+fn parse(input: &str) -> IResult<&str, HashMap<IVec2, Object>> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(
+        "########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<",
+        "2028"
+    )]
+    #[case(
+        "##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+        "10092"
+    )]
+
+    fn test_process(#[case] input: &str, #[case] result: &str) -> miette::Result<()> {
+        assert_eq!(result, process(input)?);
+        Ok(())
+    }
+}
