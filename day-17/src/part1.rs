210fe6514c1f41566b583a5ff645b1f40fbadf75
Wolves <cstingl@protonmail.com>
2024-12-17 14:19:10 -0500

    day-17(part1): Prep for working on solution

---
+++ day-17/src/part1.rs
@@ -0,0 +1,20 @@
+pub fn process(input: &str) -> miette::Result<String> {
+    let input = input.trim();
+    todo!("day 00 - part 1");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0";
+        assert_eq!("4,6,3,5,6,3,5,2,1,0", process(input)?);
+        Ok(())
+    }
+}
