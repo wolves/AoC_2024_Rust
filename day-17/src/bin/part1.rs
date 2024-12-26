11b9b6f118a7d4c5c72d1b8006483c96b57766dc
Wolves <cstingl@protonmail.com>
2024-12-26 15:57:43 -0500

    day-17(part1_2): Alternate part1 solution w/ prep Part2

--- day-17/src/bin/part1.rs
+++ day-17/src/bin/part1.rs
@@ -1,9 +1,9 @@
- use day_17::part1::process;
- use miette::Context;
+use day_17::part1_2::process;
+use miette::Context;
 
- fn main() -> miette::Result<()> {
-     let file = include_str!("../../input1.txt");
-     let result = process(file).context("process part 1")?;
-     println!("{}", result);
-     Ok(())
- }
+fn main() -> miette::Result<()> {
+    let file = include_str!("../../input1.txt");
+    let result = process(file).context("process part 1")?;
+    println!("{}", result);
+    Ok(())
+}
--- day-17/src/lib.rs
+++ day-17/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod part1;
+pub mod part1_2;
 pub mod part2;
