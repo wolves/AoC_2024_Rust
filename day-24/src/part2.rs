8d01d808dbfe09eef09bd66d224ad6c2eba92077
Wolves <cstingl@protonmail.com>
2025-01-01 17:01:58 -0500

    day-24(part2): Solution

--- day-24/src/part2.rs
+++ day-24/src/part2.rs
@@ -17,30 +17,74 @@ use nom::{
 
 pub fn process(input: &str) -> miette::Result<String> {
     let input = input.trim();
-    let (_input, (map, gates)) =
+    let (_input, (_map, gates)) =
         parse(input).map_err(|e| {
             miette::miette!("parsing failed {}", e)
         })?;
-    dbg!(&gates);
 
     let connection_cache: HashSet<_> = gates
         .iter()
         .flat_map(
             |Gate {
                  inputs,
-                 output,
+                 output: _,
                  opertaion,
              }| {
                 [
-                    (inputs[0].clone(), opertaion),
-                    (inputs[1].clone(), opertaion),
+                    (inputs[0], opertaion),
+                    (inputs[1], opertaion),
                 ]
             },
         )
         .collect();
 
-    dbg!(connection_cache);
-    todo!()
+    let mut results: Vec<_> = gates
+        .iter()
+        .filter_map(
+            |Gate {
+                 inputs,
+                 output,
+                 opertaion,
+             }| match opertaion {
+                Operation::AND
+                    if inputs[0] != "x00"
+                        && inputs[1] != "x00"
+                        && !connection_cache.contains(
+                            &(output, &Operation::OR),
+                        ) =>
+                {
+                    Some(*output)
+                }
+                Operation::XOR
+                    if ((inputs[0].starts_with('x')
+                        || inputs[1].starts_with('x'))
+                        && inputs[0] != "x00"
+                        && inputs[1] != "x00"
+                        && !connection_cache.contains(
+                            &(output, &Operation::XOR),
+                        ))
+                        || (!output.starts_with('z')
+                            && !inputs[0]
+                                .starts_with('x')
+                            && !inputs[1]
+                                .starts_with('x')) =>
+                {
+                    Some(*output)
+                }
+                Operation::OR
+                    if output.starts_with('z')
+                        && *output != "z45" =>
+                {
+                    Some(*output)
+                }
+                _ => None,
+            },
+        )
+        .collect();
+
+    results.sort();
+
+    Ok(results.join(","))
 }
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq)]
@@ -103,77 +147,4 @@ fn parse(
     let map = map.into_iter().collect();
 
     Ok((input, (map, gates)))
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_process() -> miette::Result<()> {
-        let input = "x00: 1
-x01: 1
-x02: 1
-y00: 0
-y01: 1
-y02: 0
-
-x00 AND y00 -> z00
-x01 XOR y01 -> z01
-x02 OR y02 -> z02";
-        assert_eq!("4", process(input)?);
-        Ok(())
-    }
-    #[test]
-    fn test_process2() -> miette::Result<()> {
-        let input = "x00: 1
-x01: 0
-x02: 1
-x03: 1
-x04: 0
-y00: 1
-y01: 1
-y02: 1
-y03: 1
-y04: 1
-
-ntg XOR fgs -> mjb
-y02 OR x01 -> tnw
-kwq OR kpj -> z05
-x00 OR x03 -> fst
-tgd XOR rvg -> z01
-vdt OR tnw -> bfw
-bfw AND frj -> z10
-ffh OR nrd -> bqk
-y00 AND y03 -> djm
-y03 OR y00 -> psh
-bqk OR frj -> z08
-tnw OR fst -> frj
-gnj AND tgd -> z11
-bfw XOR mjb -> z00
-x03 OR x00 -> vdt
-gnj AND wpb -> z02
-x04 AND y00 -> kjc
-djm OR pbm -> qhw
-nrd AND vdt -> hwm
-kjc AND fst -> rvg
-y04 OR y02 -> fgs
-y01 AND x02 -> pbm
-ntg OR kjc -> kwq
-psh XOR fgs -> tgd
-qhw XOR tgd -> z09
-pbm OR djm -> kpj
-x03 XOR y03 -> ffh
-x00 XOR y04 -> ntg
-bfw OR bqk -> z06
-nrd XOR fgs -> wpb
-frj XOR qhw -> z04
-bqk OR frj -> z07
-y03 OR x01 -> nrd
-hwm AND bqk -> z03
-tgd XOR rvg -> z12
-tnw OR pbm -> gnj";
-        assert_eq!("2024", process(input)?);
-        Ok(())
-    }
 }
