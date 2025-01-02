d2610d957104018eefbd62175c285d7e2f7c52e3
Wolves <cstingl@protonmail.com>
2025-01-02 16:37:30 -0500

    2015 day-01: Part 1 & 2 Solutions

--- 2015/day-01/src/part1.rs
+++ 2015/day-01/src/part1.rs
@@ -1,6 +1,16 @@
 pub fn process(input: &str) -> miette::Result<String> {
     let input = input.trim();
-    todo!("day 00 - part 1");
+
+    let mut count = 0;
+    for c in input.chars() {
+        match c {
+            '(' => count += 1,
+            ')' => count -= 1,
+            _ => unreachable!(),
+        }
+    }
+
+    Ok(count.to_string())
 }
 
 #[cfg(test)]
--- 2015/day-01/src/part2.rs
+++ 2015/day-01/src/part2.rs
@@ -1,17 +1,35 @@
 pub fn process(input: &str) -> miette::Result<String> {
     let input = input.trim();
-    todo!("day 00 - part 2");
+
+    let result = input
+        .chars()
+        .enumerate()
+        .scan(0, |count, (i, c)| {
+            *count += match c {
+                '(' => 1,
+                ')' => -1,
+                _ => unreachable!(),
+            };
+            Some((*count, i + 1))
+        })
+        .find(|(count, _)| *count == -1)
+        .map(|(_, i)| i)
+        .unwrap();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_process() -> miette::Result<()> {
-        todo!("TODO: Build Test");
-        let input = "";
-        assert_eq!("", process(input)?);
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(")", "1")]
+    #[case("()())", "5")]
+    fn test_process(#[case] input: &str, #[case] result: &str) -> miette::Result<()> {
+        assert_eq!(result, process(input)?);
         Ok(())
     }
 }
