fa1c07d541dc30cf1bd1bb2c85fc5a46f692b886
Wolves <cstingl@protonmail.com>
2025-01-02 10:11:44 -0500

    day-25(part1): Solution

--- day-25/src/lib.rs
+++ day-25/src/lib.rs
@@ -1,2 +1,3 @@
+#![feature(extract_if)]
 pub mod part1;
 pub mod part2;
--- day-25/src/part1.rs
+++ day-25/src/part1.rs
@@ -1,6 +1,115 @@
+use itertools::Itertools;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::line_ending,
+    combinator::{iterator, opt, peek},
+    multi::separated_list1,
+    sequence::{preceded, terminated, tuple},
+    IResult,
+};
+
 pub fn process(input: &str) -> miette::Result<String> {
     let input = input.trim();
-    todo!("day 00 - part 1");
+    let (_input, mut all) = parse(input).map_err(|e| {
+        miette::miette!("parsing failed {}", e)
+    })?;
+
+    let locks = all
+        .extract_if(.., |device| {
+            device.r#type == DeviceType::Lock
+        })
+        .collect::<Vec<Device>>();
+    let keys = all;
+
+    let result = locks
+        .iter()
+        .cartesian_product(keys.iter())
+        .filter(|(lock, key)| {
+            std::iter::zip(lock.pins, key.pins)
+                .all(|(a, b)| a + b <= 5)
+        })
+        .count();
+
+    Ok(result.to_string())
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum DeviceType {
+    Lock,
+    Key,
+}
+
+fn accumulate_pins(
+    mut pins: [i32; 5],
+) -> impl FnMut(&str) -> IResult<&str, [i32; 5]> {
+    move |input| {
+        let mut it = iterator(
+            input,
+            terminated(
+                alt((tag("#"), tag("."))),
+                opt(line_ending),
+            ),
+        );
+
+        for (i, value) in it.enumerate() {
+            pins[i % 5] += match value {
+                "#" => 1,
+                _ => 0,
+            };
+        }
+
+        let res: IResult<_, _> = it.finish();
+
+        res.map(|(input, _)| (input, pins))
+    }
+}
+
+#[derive(Debug)]
+struct Device {
+    r#type: DeviceType,
+    pins: [i32; 5],
+}
+
+fn lock(input: &str) -> IResult<&str, Device> {
+    let (input, _) = tag("#####")(input)?;
+
+    let (input, pins) = preceded(
+        tuple((
+            line_ending,
+            peek(alt((tag("."), tag("#")))),
+        )),
+        accumulate_pins([0i32; 5]),
+    )(input)?;
+    Ok((
+        input,
+        Device {
+            pins,
+            r#type: DeviceType::Lock,
+        },
+    ))
+}
+
+fn key(input: &str) -> IResult<&str, Device> {
+    let (input, _) = tag(".....")(input)?;
+    let (input, pins) = preceded(
+        tuple((
+            line_ending,
+            peek(alt((tag("."), tag("#")))),
+        )),
+        accumulate_pins([-1i32; 5]),
+    )(input)?;
+    Ok((
+        input,
+        Device {
+            pins,
+            r#type: DeviceType::Key,
+        },
+    ))
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<Device>> {
+    separated_list1(line_ending, alt((key, lock)))(input)
 }
 
 #[cfg(test)]
