df57d6d644a116713a36b978e8d0b5093bbfc546
wolves <cstingl@protonmail.com>
2025-01-02 19:40:13 -0500

    day-02(part1): Setup struct, parsing and surface_area method

--- 2015/day-02/src/part1.rs
+++ 2015/day-02/src/part1.rs
@@ -1,8 +1,47 @@
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, line_ending},
+    multi::separated_list1,
+    IResult,
+};
+
 pub fn process(input: &str) -> miette::Result<String> {
     let input = input.trim();
+    let (_input, presents) = parse(input).map_err(|e| miette::miette!("Parsing failed {}", e))?;
 
     //2*l*w + 2*w*h + 2*h*l
     todo!("day 00 - part 1");
+}
+
+#[derive(Debug)]
+struct Present {
+    l: u32,
+    w: u32,
+    h: u32,
+}
+
+impl Present {
+    fn surface_area(&self) -> u32 {
+        let Present { l, w, h } = self;
+        (2 * l * w) + (2 * w * h) + (2 * h * l)
+    }
+}
+
+fn present(input: &str) -> IResult<&str, Present> {
+    let (input, dimensions) = separated_list1(tag("x"), complete::u32)(input)?;
+
+    Ok((
+        input,
+        Present {
+            l: dimensions[0],
+            w: dimensions[1],
+            h: dimensions[2],
+        },
+    ))
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<Present>> {
+    separated_list1(line_ending, present)(input)
 }
 
 #[cfg(test)]
