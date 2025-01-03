04ff51d58ff5fecc1eb384738c6b65ff482cdf16
Wolves <cstingl@protonmail.com>
2025-01-03 13:42:26 -0500

    day-03(part1): Solution along with location count hashmap
    - Anticipated needing the count of stops per location for part2 but
      ended up being wrong so the HashMap count of locations ended up being
      unnecessary

--- 2015/day-03/src/part1.rs
+++ 2015/day-03/src/part1.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 use glam::IVec2;
+use itertools::Itertools;
 use nom::{
     branch::alt,
     character::complete::{self, line_ending},
@@ -11,8 +14,21 @@ pub fn process(input: &str) -> miette::Result<String> {
     let input = input.trim();
     let (_input, directions) = parse(input).map_err(|e| miette::miette!("Parsing failed {e}"))?;
 
-    dbg!(directions);
-    todo!("day 00 - part 1");
+    let mut visited: Vec<IVec2> = vec![IVec2::new(0, 0)];
+    let mut location_stops = HashMap::from([(visited[0], 1)]);
+
+    for direction in directions {
+        let next_location = visited.last().unwrap() + direction;
+        visited.push(next_location);
+
+        location_stops
+            .entry(next_location)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+    }
+
+    let result = visited.iter().unique().count();
+    Ok(result.to_string())
 }
 
 fn parse(input: &str) -> IResult<&str, Vec<IVec2>> {
