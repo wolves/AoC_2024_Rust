1180953ea4a24e85236ec174c552388a0dc44e16
wolves <cstingl@protonmail.com>
2025-01-04 09:22:29 -0500

    day-04(part2): Extremely slow brute force solution

--- 2015/day-04/src/part2.rs
+++ 2015/day-04/src/part2.rs
@@ -1,17 +1,22 @@
 pub fn process(input: &str) -> miette::Result<String> {
     let input = input.trim();
-    todo!("day 00 - part 2");
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    let mut buffer = String::with_capacity(input.len() + 20);
+    let mut i = 0;
 
-    #[test]
-    fn test_process() -> miette::Result<()> {
-        todo!("TODO: Build Test");
-        let input = "";
-        assert_eq!("", process(input)?);
-        Ok(())
-    }
+    let result = loop {
+        i += 1;
+
+        buffer.clear();
+        buffer.push_str(input);
+        buffer.push_str(&i.to_string());
+
+        let hash = md5::compute(buffer.as_bytes());
+
+        if format!("{:x}", hash).as_bytes().starts_with(b"000000") {
+            break i;
+        }
+    };
+
+    Ok(result.to_string())
 }
