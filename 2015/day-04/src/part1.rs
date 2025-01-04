e6982125a92f762e1d9b0dd6dbee55595650d6b8
wolves <cstingl@protonmail.com>
2025-01-04 09:17:54 -0500

    day-04(part1): Rework for less allocations in the hot loop

--- 2015/day-04/src/part1.rs
+++ 2015/day-04/src/part1.rs
@@ -1,14 +1,19 @@
 pub fn process(input: &str) -> miette::Result<String> {
     let input = input.trim();
 
+    let mut buffer = String::with_capacity(input.len() + 20);
     let mut i = 0;
+
     let result = loop {
         i += 1;
 
-        let hash = md5::compute(format!("{input}{i}"));
-        let hex = format!("{:x}", hash);
+        buffer.clear();
+        buffer.push_str(input);
+        buffer.push_str(&i.to_string());
 
-        if hex.starts_with("00000") {
+        let hash = md5::compute(buffer.as_bytes());
+
+        if format!("{:x}", hash).as_bytes().starts_with(b"00000") {
             break i;
         }
     };
