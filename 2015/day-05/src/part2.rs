47ce463ae7a317c2d4ef44527dfd1212cba2d2ed
wolves <cstingl@protonmail.com>
2025-01-05 19:49:09 -0500

    day-05(part2): Solution

--- 2015/day-05/src/part2.rs
+++ 2015/day-05/src/part2.rs
@@ -1,17 +1,44 @@
 pub fn process(input: &str) -> miette::Result<String> {
     let input = input.trim();
-    todo!("day 00 - part 2");
+
+    let result = input
+        .lines()
+        .filter(|line| {
+            line.as_bytes()
+                .windows(2)
+                .enumerate()
+                .any(|(i, current_pair)| {
+                    line.as_bytes()
+                        .windows(2)
+                        .skip(i + 2)
+                        .any(|next_pair| current_pair == next_pair)
+                })
+        })
+        .filter(|line| line.as_bytes().windows(3).any(|win| win[0] == win[2]))
+        .count();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
-    #[test]
-    fn test_process() -> miette::Result<()> {
-        todo!("TODO: Build Test");
-        let input = "";
-        assert_eq!("", process(input)?);
+    #[rstest]
+    #[case("qjhvhtzxzqqjkmpb", "1")]
+    #[case("xxyxx", "1")]
+    #[case("uurcxstgmygtbstg", "0")]
+    #[case("ieodomkazucvgmuy", "0")]
+    #[case(
+        "qjhvhtzxzqqjkmpb
+xxyxx
+uurcxstgmygtbstg
+ieodomkazucvgmuy",
+        "2"
+    )]
+    fn test_process(#[case] input: &str, #[case] result: &str) -> miette::Result<()> {
+        assert_eq!(result, process(input)?);
         Ok(())
     }
 }
