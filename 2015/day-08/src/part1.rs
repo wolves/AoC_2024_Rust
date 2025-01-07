9eb8dc8b46d43aae3c053d0aca0cd10668069289
Wolves <cstingl@protonmail.com>
2025-01-07 13:05:03 -0500

    day-08(part1): Solution

--- 2015/day-08/src/part1.rs
+++ 2015/day-08/src/part1.rs
@@ -1,38 +1,32 @@
+//! [Solution Credit](https://github.com/maneatingape/advent-of-code-rust/blob/main/src/year2015/day08.rs)
+//!
+//! Nom parsing or my shallow depth of understanding of it did not work for me for this day
+//!
+//! Explanation from maneatingape original solution:
+//!
+//! While [regular expressions](https://en.wikipedia.org/wiki/Regular_expression) may feel like a
+//! natural choice, it's much faster and easier to simply treat the input as a stream of raw
+//! ASCII `u8` bytes including newlines.
+//!
+//! For part one we run a small state machine using [`fold`] to keep track of the current and
+//! previous characters. If we encounter a hexadecimal escape then four characters become one so the
+//! difference increases by three. The sequences `\\` and `\"` both increase the difference by one.
+//! Each newline increases the difference by two since every line is enclosed with two quotes.
+
+const NEWLINE: u8 = 10;
+const SLASH: u8 = 92;
+const ESCAPE: u8 = 120;
+
 pub fn process(input: &str) -> miette::Result<String> {
-    let input = input.trim();
+    let (_, result) = input
+        .bytes()
+        .fold((false, 0), |(flag, count), b| match (flag, b) {
+            (true, ESCAPE) => (false, count + 3),
+            (true, _) => (false, count + 1),
+            (false, SLASH) => (true, count),
+            (false, NEWLINE) => (false, count + 2),
+            _ => (false, count),
+        });
 
-    dbg!(input, input.len());
-    todo!("day 00 - part 1");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rstest::rstest;
-
-    #[rstest]
-    #[case(r#""""#, 2, 0)]
-    #[case(r#""abc""#, 5, 3)]
-    #[case(r#""aaa\"aaa""#, 10, 7)]
-    #[case(r#""\x27""#, 6, 1)]
-    fn test_string_processing(
-        #[case] input: &str,
-        #[case] code_len: usize,
-        #[case] mem_len: usize,
-    ) {
-        let result = process_string(input);
-        assert_eq!(result.code_chars, code_len);
-        assert_eq!(result.memory_chars, mem_len);
-    }
-
-    #[test]
-    fn test_total_diff() {
-        let input = vec![r#""""#, r#""abc""#, r#""aaa\"aaa""#, r#""\x27""#];
-
-        assert_eq!(calc_diff(&input), 12)
-    }
-    // fn test_process(#[case] input: &str, #[case] result: &str) -> miette::Result<()> {
-    //     assert_eq!(result, process(input)?);
-    //     Ok(())
-    // }
+    Ok(result.to_string())
 }
