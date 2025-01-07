d328f329282113ab4c21c4259a19f7b49d1b1f1f
Wolves <cstingl@protonmail.com>
2025-01-07 13:28:51 -0500

    day-08(part2): Solution

--- 2015/day-08/src/part2.rs
+++ 2015/day-08/src/part2.rs
@@ -1,17 +1,27 @@
+//! [Solution Credit](https://github.com/maneatingape/advent-of-code-rust/blob/main/src/year2015/day08.rs)
+//!
+//! Nom parsing or my shallow depth of understanding of it did not work for me for this day
+//!
+//! Explanation from maneatingape original solution:
+//!
+//! Part two is even more straightforward with no need for statekeeping. Quotes and backslashes
+//! need to be escaped so increase the difference by one. As before each newline increases by the
+//! difference by two.
+
+const NEWLINE: u8 = 10;
+const QUOTE: u8 = 34;
+const SLASH: u8 = 92;
+const ESCAPE: u8 = 120;
+
 pub fn process(input: &str) -> miette::Result<String> {
-    let input = input.trim();
-    todo!("day 00 - part 2");
-}
+    let result: usize = input
+        .bytes()
+        .map(|b| match b {
+            QUOTE | SLASH => 1,
+            NEWLINE => 2,
+            _ => 0,
+        })
+        .sum();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_process() -> miette::Result<()> {
-        todo!("TODO: Build Test");
-        let input = "";
-        assert_eq!("", process(input)?);
-        Ok(())
-    }
+    Ok(result.to_string())
 }
