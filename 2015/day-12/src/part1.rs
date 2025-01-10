3f87d8b7725d1ca3e6e6b989108178885b239ac9
Wolves <cstingl@protonmail.com>
2025-01-10 14:35:28 -0500

    day-12(part1): Add tests

--- 2015/day-12/src/part1.rs
+++ 2015/day-12/src/part1.rs
@@ -9,7 +9,14 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case("", "")]
+    #[case("[1,2,3]", "6")]
+    #[case(r#"{"a":2,"b":4}"#, "6")]
+    #[case("[[[3]]]", "3")]
+    #[case(r#"{"a":{"b":4},"c":-1}"#, "3")]
+    #[case(r#"{"a":[-1,1]}"#, "0")]
+    #[case(r#"[-1,{"a":1}]"#, "0")]
+    #[case("[]", "0")]
+    #[case("{}", "0")]
     fn test_process(#[case] input: &str, #[case] result: &str) -> miette::Result<()> {
         assert_eq!(result, process(input)?);
         Ok(())
